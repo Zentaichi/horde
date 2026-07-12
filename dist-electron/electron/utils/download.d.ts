11876945f468b1b491c2bfe42762d55443dfbe8e
Ernest Endrino <ernest.endrino@gmail.com>
2026-07-13 00:53:56 +0800

    chore: delete tracked dist build folder

--- dist-electron/electron/utils/download.d.ts
+++
@@ -1,6 +0,0 @@
-export interface ProgressInfo {
-    percent: number;
-    transferredBytes: number;
-    totalBytes: number;
-}
-export declare function downloadFile(url: string, destPath: string, onProgress?: (info: ProgressInfo) => void): Promise<void>;
